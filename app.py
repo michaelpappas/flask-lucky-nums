e8ae526ddd9c2ff4b8631c30dab3b4f3e149da8d
michaelpappas <pappas.michael@gmail.com>
2022-12-17 14:53:46 -0800

    api working with errors correctly in insomnia

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import random, requests
 
 app = Flask(__name__)
 
@@ -8,3 +9,71 @@ def homepage():
     """Show homepage."""
 
     return render_template("index.html")
+
+@app.post("/api/get-lucky-num")
+def return_lucky_num():
+    """ Return JSON of all number and year data {"num": {"fact": "...",
+                                                 "num": <integer>
+                                                 },
+                                                "year": {"fact": "...",
+                                                    "year": <integer>}
+                                                    }
+    """
+    year = request.json['year']
+    errors = error_check(request.json)
+
+    number_data = num_request()
+    year_data = year_request(year)
+
+    if errors:
+        return jsonify(errors), 200
+    return jsonify(number_data, year_data)
+
+def error_check(data):
+    resp = {}
+    resp['errors'] = {}
+    try:
+        data['name']
+    except:
+        resp['errors']['name']=["This field is required."]
+
+    try:
+        data['email']
+    except:
+        resp['errors']['email']=["This field is required."]
+
+    try:
+        data['year']
+    except:
+        resp['errors']['year']=["This field is required."]
+
+    try:
+        data['color']
+    except:
+        resp['errors']['color']=["This field is required."]
+
+    if data['color'] not in ['red', 'green', 'orange', 'blue']:
+        resp['errors']['color']=["Invalid value, must be one of: red, green, orange, blue."]
+    if resp['errors'] == {}:
+        return None
+    else:
+        return resp
+
+def num_request():
+    """ generates and random number and returns JSON of number and fact about number"""
+
+    rand_num = random.randrange(0,100)
+    num_fact = requests.get(f"http://numbersapi.com/{rand_num}/math")
+
+    return {"num":{"fact": num_fact.text,
+                "num": rand_num}
+                }
+
+def year_request(year):
+    """ generates and random number and returns JSON of number and fact about number"""
+
+    year_fact = requests.get(f"http://numbersapi.com/{year}/year")
+
+    return {"year":{"fact": year_fact.text,
+                    "year": year}
+                    }
--- static/lucky.js
+++ static/lucky.js
@@ -4,8 +4,14 @@
  * - show errors, if errors are returned
  * - else: show results
  **/
+const $form = $("form")
 
 async function processForm(evt) {
+  evt.preventDefault()
+
+  const formValues = getFormValues(evt);
+  resp = await postForm(formValues)
+  console.log(resp)
 }
 
 /** showErrors: show error messages in DOM. */
@@ -16,6 +22,19 @@ function showErrors(errors) {
 /** showResults: show num and year in the DOM. */
 
 function showResults(num, year) {
+}
+function getFormValues(evt) {
+  const name = evt.target.name.value;
+  const year = evt.target.year.value;
+  const email = evt.target.email.value;
+  const color = evt.target.color.value;
+
+  return {name: name, year: year, email: email, color: color};
+}
+
+async function postForm(values){
+  response = await axios.post('http://localhost:5001/api/get-lucky-num', values)
+  return response.data
 }
 
 
