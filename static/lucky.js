7c30b131af6411320ecf4507eb7966aa2996b1e6
michaelpappas <pappas.michael@gmail.com>
2022-12-16 18:48:31 -0800

    initial commit

---
+++ static/lucky.js
@@ -0,0 +1,22 @@
+/** processForm: handle submission of form:
+ *
+ * - make API call to server to get num/year
+ * - show errors, if errors are returned
+ * - else: show results
+ **/
+
+async function processForm(evt) {
+}
+
+/** showErrors: show error messages in DOM. */
+
+function showErrors(errors) {
+}
+
+/** showResults: show num and year in the DOM. */
+
+function showResults(num, year) {
+}
+
+
+$("#lucky-form").on("submit", processForm);
