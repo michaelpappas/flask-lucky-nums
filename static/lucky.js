bed27ea043b6a8dbbb38cd4808aca4bdb7199dfc
michaelpappas <pappas.michael@gmail.com>
2022-12-17 18:05:19 -0800

    functionality complete, refactor next

--- static/lucky.js
+++ static/lucky.js
@@ -5,30 +5,60 @@
  * - else: show results
  **/
 const $form = $("form")
+const $nameErr = $("#name-err")
+const $emailErr = $("#email-err")
+const $yearErr = $("#year-err")
+const $colorErr = $("#color-err")
+const $results = $("#lucky-results")
 
 async function processForm(evt) {
   evt.preventDefault()
 
   const formValues = getFormValues(evt);
   resp = await postForm(formValues)
-  console.log(resp)
+  debugger;
+  if(resp.errors){
+    showErrors(resp)
+  }
+  else{
+    showResults(resp[0].num, resp[1].year)
+  }
+
 }
 
 /** showErrors: show error messages in DOM. */
 
-function showErrors(errors) {
+function showErrors(input){
+  if (input.errors){
+    for (let error in input.errors){
+      if (error === "name"){
+        $nameErr.text(input.errors[error])
+      }
+      if (error === "year"){
+        $yearErr.text(input.errors[error])
+      }
+      if (error === "email"){
+        $emailErr.text(input.errors[error])
+      }
+      if (error === "color"){
+        $colorErr.text(input.errors[error])
+      }
+    }
+  }
 }
 
 /** showResults: show num and year in the DOM. */
 
 function showResults(num, year) {
+  $results.html(`<p>Your lucky number is ${num.num} (${num.fact})</p>
+                <p> Your birth year (${year.year}) fact is ${year.fact}</p>`)
 }
+/** Retrieves  */
 function getFormValues(evt) {
   const name = evt.target.name.value;
   const year = evt.target.year.value;
   const email = evt.target.email.value;
   const color = evt.target.color.value;
-  debugger;
   return {name: name, year: year, email: email, color: color};
 }
 
@@ -36,6 +66,8 @@ async function postForm(values){
   response = await axios.post('http://localhost:5001/api/get-lucky-num', values)
   return response.data
 }
+
+
 
 
 $("#lucky-form").on("submit", processForm);
